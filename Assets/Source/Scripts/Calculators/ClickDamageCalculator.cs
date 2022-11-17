e243ea13e95fd34877c8f225ad9f64e14a97e75a
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-11-17 20:53:52 +0300

    Create criticalChanceCalculator

--- Assets/Source/Scripts/Calculators/ClickDamageCalculator.cs
+++ Assets/Source/Scripts/Calculators/ClickDamageCalculator.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class ClickDamageCalculator : MonoBehaviour
 {
     [SerializeField] private PlayerLevel _playerLevel;
+    [SerializeField] private CriticalStrikeCalculator _critical;
 
     public double GetValue()
     {
-        return _playerLevel.Value;
+        return _playerLevel.Value * _critical.GetValue();
     }
 }
---
+++ Assets/Source/Scripts/Calculators/CriticalStrikeCalculator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriticalStrikeCalculator : MonoBehaviour
+{
+    private float _criticalStrikeChance = 1;
+    private float _criticalStrikeMultiplicator = 1.5f;
+
+    public float GetValue()
+    {
+        bool isCritical = Random.Range(1, 100) <= _criticalStrikeChance;
+
+        if (isCritical == true)
+            return _criticalStrikeMultiplicator;
+        else
+            return 1;
+    }
+}
