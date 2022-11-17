5d74a3a5fc33b52736c8cbe43a4e45c069489542
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-11-17 17:34:39 +0300

    Add ButtonNextVawe

--- Assets/Source/Scripts/Vawe/VaweCounter.cs
+++ Assets/Source/Scripts/Vawe/VaweCounter.cs
@@ -1,11 +1,11 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VaweCounter : MonoBehaviour
 {
     [SerializeField] private EnemySpawner _spawner;
+    [SerializeField] private Button _buttonNextVawe;
 
     private int _vawe;
 
@@ -16,21 +16,30 @@ public class VaweCounter : MonoBehaviour
     private void OnEnable()
     {
         _spawner.VaweFinished += OnVaweFinished;
+        _buttonNextVawe.onClick.AddListener(OnButtonNextVaweClick);
     }
 
     private void Start()
     {
-        VaweStarted?.Invoke();   
+        VaweStarted?.Invoke();
+        _buttonNextVawe.gameObject.SetActive(false);
     }
 
     private void OnDisable()
     {
         _spawner.VaweFinished -= OnVaweFinished;
+        _buttonNextVawe.onClick.RemoveListener(OnButtonNextVaweClick);
     }
 
     private void OnVaweFinished()
     {
         _vawe++;
+        _buttonNextVawe.gameObject.SetActive(true);
+    }
+
+    private void OnButtonNextVaweClick()
+    {
         VaweStarted?.Invoke();
+        _buttonNextVawe.gameObject.SetActive(false);
     }
 }
