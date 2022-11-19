d70eb03534ddfbb152501c9716e7b608bec504a0
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-11-19 20:38:05 +0300

    Create new Tower prefab

---
+++ Assets/Source/Scripts/Towers/TowerUpgrader.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerUpgrader : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        print("Yep Upgrade avaliable");
+    }
+}
