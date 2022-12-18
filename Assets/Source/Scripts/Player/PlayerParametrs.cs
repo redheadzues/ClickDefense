d754743f2f034e1682719f80ce44d87eaca5bdbf
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-12-19 00:39:38 +0300

    Update PlayerParametrs.cs

--- Assets/Source/Scripts/Player/PlayerParametrs.cs
+++ Assets/Source/Scripts/Player/PlayerParametrs.cs
@@ -1,14 +1,23 @@
-public class PlayerParametrs 
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using NumbersForIdle;
 
-    // Update is called once per frame
-    void Update()
+namespace Player
+{
+    public class PlayerParametrs
     {
-        
+        private int _level;
+        private IdleNumber _damage;
+        private float _criticalChance;
+        private float _criticalMultiplicator;
+
+        public int Level => _level;
+        public IdleNumber Damage => _damage;
+        public float CriticalChance => _criticalChance; 
+        public float CriticalMultiplicator => _criticalMultiplicator;
+
+        public PlayerParametrs()
+        {
+
+        }
+
     }
 }
