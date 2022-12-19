6c6e814d521b5a4e56e23da7d2c30ba4d0fd1c8d
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-12-19 11:34:01 +0300

    delete playerlevel

--- Assets/Source/Scripts/Player/PlayerModel.cs
+++ Assets/Source/Scripts/Player/PlayerModel.cs
@@ -3,14 +3,12 @@ namespace Player
     public class PlayerModel
     {
         private Wallet _wallet;
-        private PlayerLevel _playerLevel;
 
         public IWallet Wallet => _wallet;
 
         public PlayerModel()
         {
             _wallet = new Wallet();
-            _playerLevel = new PlayerLevel();
         }
     }
 
