150ab8f54eb32d9b38f7c8bf5d4ac30d6f30873e
redheadzues <48310838+redheadzues@users.noreply.github.com>
2023-01-19 20:06:35 +0300

    Create EnemyFactory

---
+++ Assets/Source/Scripts/Infrustructure/Services/Factories/EnemyFactory.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Source.Scripts.Infrustructure.Services.AssetManagment;
+using Assets.Source.Scripts.Infrustructure.Services.ClickListener;
+using UnityEngine;
+
+namespace Assets.Source.Scripts.Infrustructure.Services.Factories
+{
+    public class EnemyFactory : IEnemyFactory
+    {
+        private readonly IAssetProvider _assetProvider;
+        private readonly IClickListener _clickListener;
+
+        public EnemyFactory(IAssetProvider assetProvider, IClickListener clickListener)
+        {
+            _assetProvider = assetProvider;
+            _clickListener = clickListener;
+        }
+
+        public void CreateEnemy()
+        {
+            GameObject enemy = _assetProvider.Instantiate(AssetPath.Enemy);
+
+            if (enemy.TryGetComponent(out ClickReader clickReader))
+                _clickListener.Register(clickReader);
+        }
+    }
+}
---
+++ Assets/Source/Scripts/Infrustructure/Services/Factories/IEnemyFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Source.Scripts.Infrustructure.Services.Factories
+{
+    public interface IEnemyFactory
+    {
+        void CreateEnemy();
+    }
+}
