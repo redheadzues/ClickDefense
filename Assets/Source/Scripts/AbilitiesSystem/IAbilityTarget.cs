6b4a1a27dd81fcf8f566aa671e73e6a6ce3a4e17
redheadzues <48310838+redheadzues@users.noreply.github.com>
2023-01-29 10:33:53 +0300

    Update IAbilityTarget.cs

--- Assets/Source/Scripts/AbilitiesSystem/IAbilityTarget.cs
+++ Assets/Source/Scripts/AbilitiesSystem/IAbilityTarget.cs
@@ -13,16 +13,29 @@ namespace Assets.Source.Scripts.AbilitiesSystem
 
     public class AbilityTargetComponent : MonoBehaviour, IAbilityTarget
     {
-        public IReadOnlyList<AbilityTag> Tags => throw new System.NotImplementedException();
-
-        public Vector3 Position => transform.position;
-
+        [SerializeField] private List<AbilityTag> _tags;
+            
         private List<GamePlayEffect> _activeEffects = new List<GamePlayEffect>();
+
+        public IReadOnlyList<AbilityTag> Tags => _tags;
+        public Vector3 Position => transform.position;
 
         public void TakeEffect(GamePlayEffect effect)
         {
             _activeEffects.Add(effect);
+            effect.Ended += OnEffectEnded;
         }
+
+        private void OnEffectEnded(GamePlayEffect effect)
+        {
+            effect.Ended -= OnEffectEnded;
+            _activeEffects.Remove(effect);
+        }
+    }
+
+    public class AttributeSet : MonoBehaviour
+    {
+
     }
 
 }
