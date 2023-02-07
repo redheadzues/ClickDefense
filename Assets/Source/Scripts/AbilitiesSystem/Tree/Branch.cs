18767f023dee7d1bcf0f2d8ebb3dad81fedebfa4
redheadzues <48310838+redheadzues@users.noreply.github.com>
2023-02-07 13:42:16 +0300

    a

---
+++ Assets/Source/Scripts/AbilitiesSystem/Tree/Branch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Source.Scripts.AbilitiesSystem.Tree
+{
+    [CreateAssetMenu(fileName = "Branch", menuName = "StaticData/Abilities/AbilityTree/Branch")]
+    public class Branch : ScriptableObject
+    {
+        public List<BranchLeaf> _branchLevels;
+    }
+}
---
+++ Assets/Source/Scripts/AbilitiesSystem/Tree/BranchLeaf.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Source.Scripts.AbilitiesSystem.StaticData;
+using UnityEngine;
+
+namespace Assets.Source.Scripts.AbilitiesSystem.Tree
+{
+    [CreateAssetMenu(fileName = "BranchLeaf", menuName = "StaticData/Abilities/AbilityTree/BranchLeaf")]
+    public class BranchLeaf : ScriptableObject
+    {
+        public AbilityStaticData abilityData;
+        public int Level;
+    }
+}
