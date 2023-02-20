141f1bf3ada93191fe894b00746d138c8c328b7b
redheadzues <48310838+redheadzues@users.noreply.github.com>
2023-02-20 20:40:35 +0300

    Create Allie static data

--- Assets/Source/Scripts/Infrustructure/StaticData/AllieStaticData.cs
+++ Assets/Source/Scripts/Infrustructure/StaticData/AllieStaticData.cs
@@ -1,17 +1,15 @@
 ﻿using Assets.Source.Scripts.AbilitiesSystem.Attributes;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Source.Scripts.Infrustructure.StaticData
 {
-    [CreateAssetMenu(fileName = "EnemyData", menuName = "StaticData/Enemy")]
-    public class EnemyStaticData : ScriptableObject
-    {        
+    [CreateAssetMenu(fileName = "AllieData", menuName = "StaticData/Allie")]
+    public class AllieStaticData : ScriptableObject
+    {
         [field: SerializeField] public EnemyTypeId EnemyTypeId { get; private set; }
         [field: SerializeField, Range(1, 100)] public int HP { get; private set; }
         [field: SerializeField, Range(1, 20)] public int Reward { get; private set; }
         [field: SerializeField] public GameObject Prefab { get; private set; }
-        [field: SerializeField] public GamePlayAttributes Attributes { get; private set; }        
-        
+        [field: SerializeField] public GamePlayAttributes Attributes { get; private set; }
     }
 }
